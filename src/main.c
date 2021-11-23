e2b812a2192c4a30ec45abba8b89f4adace7a7ac
Jonas Geertsen Lund <jgl21@student.aau.dk>
2021-11-23 11:19:37 +0100

    New changes to title page

--- src/main.c
+++ src/main.c
@@ -1,14 +1,15 @@
 /* 
+ *  Title:          Sustainable Webbrowsing
+ *  Group:          SW B222
+ *  Names:          Andreas, Ali, Alexander, Viktor, Frederik, Thorbjørn & Jonas.
+ *  Supervisor:     Morten Konggard Schou
  *  Initial commit: 23/11-2021
- *  Group: SW B222
- *  Names:  Andreas, Ali, Alexander, Viktor, Frederik, Thorbjørn & Jonas.
- *  Supervisor: Morten Konggard Schou
  *  Date completed: ??/12-2021
- *  Main function - add a description HERE.
- *  ....
- *  ....
+ *  Description:
+ *      Add description here ...
+ *      ....
+ *      ....
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 
