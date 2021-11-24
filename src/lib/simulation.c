b02d0afff77c6d0ee85d7a241accadf407aa1ec8
Viktor Platz <redviktor10@hotmail.com>
2021-11-24 14:14:59 +0100

    Added quick comment

--- src/lib/simulation.c
+++ src/lib/simulation.c
@@ -1,6 +1,6 @@
 #include "simulation.h"
 
 SimulationInput get_sim_input() {
-    SimulationInput sim_input = {10, 3600, 10, 7200};
+    SimulationInput sim_input = {10, 3600, 10, 7200}; /* num_users, avg_user_time, time_increment, sim_duration */
     return sim_input;
 }
