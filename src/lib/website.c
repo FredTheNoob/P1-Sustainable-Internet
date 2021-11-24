b7d42095a685a7240404266085d6fb7323ebb958
FredTheNoob <frederikhammer02@gmail.com>
2021-11-24 15:23:09 +0100

    Implemented get_website in website.c
    Co-authored-by: AlexanderManich <AlexanderManich@users.noreply.github.com>

--- src/lib/website.c
+++ src/lib/website.c
@@ -1,1 +1,18 @@
 #include "website.h"
+
+Website *get_website(Website *websites, unsigned int websites_length, short previous_website_id) {
+    /* Roll a dice between 0 and 100 */
+    int roll = rand() % 100;
+
+    /* Loop over websites */
+    int curr_range = 0;
+    for (int i = 0; i < websites_length; i++) {
+        if (roll > curr_range && roll <= websites[i].influence + curr_range
+            && websites[i].id != previous_website_id) {
+            return &websites[i];
+        }   
+        else {
+            curr_range += websites[i].influence;
+        }
+    }
+}
--- src/lib/website.h
+++ src/lib/website.h
@@ -2,13 +2,13 @@
 #define WEBSITE
 
 struct Website {
-    unsigned short id;
+    short id;
     unsigned short avg_duration;
     float pages_per_visit;
     float influence;
 };
 typedef struct Website Website;
 
-Website *get_website(Website *websites, unsigned short previous_website_id);
+Website *get_website(Website *websites, unsigned int websites_length, short previous_website_id);
 
 #endif
