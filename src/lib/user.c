37598220803de8cc4a3aa1d0d4e7242bc26253ea
Viktor Platz <62332353+redviktor@users.noreply.github.com>
2021-12-13 14:52:17 +0100

    Quick comment :-)
    Co-authored-by: Ali Sajad Khorami <sabotack@users.noreply.github.com>
    Co-authored-by: Thorbjørn Møller <thor1878@users.noreply.github.com>

--- src/lib/user.c
+++ src/lib/user.c
@@ -26,7 +26,7 @@ void handle_user(User *user, Website *websites, WebsiteNode **linked_websites, c
             website = get_website(websites, NUM_WEBSITES, user->current_website->id); /* Returns a new website */
         }
 
-        /*  */
+        /* Find out if it is possible to recommend a website to the user, which the user has not  */
 
             
         /* Recommend a more sustainable website based on the initial website */
